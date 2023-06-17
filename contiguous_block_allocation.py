96b4ff93f12c7cc824e38046ebc6e0ba712f8079
ashik190141 <ashanmahmudashik@gmail.com>
2023-06-17 19:08:34 +0600

    contiguous

---
+++ contiguous_block_allocation.py
@@ -0,0 +1,24 @@
+print(f'Enter the number of files: ',end='')
+n = int(input())
+fileInfo = []
+
+for i in range(n):
+    print(f'Enter the file {i+1}: ',end='')
+    fileName = input()
+    print(f'Enter the starting block of file {i+1}: ',end='')
+    startingBlock = int(input())
+    print(f'Enter the length of the block {i+1}: ',end='')
+    lengthOfBlock = int(input())
+    fileInfo.append((fileName,startingBlock,lengthOfBlock))
+
+print('Enter the search file name: ',end='')
+searchFile = input()
+for i in range(n):
+    if(searchFile == fileInfo[i][0]):
+        si = fileInfo[i][1]
+        length = fileInfo[i][2]
+        break
+
+for i in range(si,si+length):
+    print(i,end=' ')
+print()
